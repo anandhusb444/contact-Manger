400c83800cd3c80fbd88f60c7655257adc0d292d
anandhusb444 <anandhusb444@gmail.com>
2025-07-18 23:16:42 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace contact_Manger.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
